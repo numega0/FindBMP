71437eba39e52ca68c4124115b68f360ec812a4d
Muharrem Bayındır <alone_wolf_2758@hotmail.com>
2019-06-14 01:18:02 +0300

    Add project files.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Otp_Stealer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.tmrSystem = new System.Windows.Forms.Timer(this.components);
+            this.listIslemler = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(541, 14);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(176, 56);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Başlat";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // tmrSystem
+            // 
+            this.tmrSystem.Tick += new System.EventHandler(this.tmrSystem_Tick);
+            // 
+            // listIslemler
+            // 
+            this.listIslemler.FormattingEnabled = true;
+            this.listIslemler.Location = new System.Drawing.Point(541, 76);
+            this.listIslemler.Name = "listIslemler";
+            this.listIslemler.Size = new System.Drawing.Size(176, 381);
+            this.listIslemler.TabIndex = 1;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(724, 469);
+            this.Controls.Add(this.listIslemler);
+            this.Controls.Add(this.btnLogin);
+            this.Name = "frmMain";
+            this.Text = "OTPÇAL";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Timer tmrSystem;
+        private System.Windows.Forms.ListBox listIslemler;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+
+
+namespace Otp_Stealer
+{
+    public partial class frmMain : Form
+    {
+        [DllImport("user32.dll")]
+        static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData,
+          int dwExtraInfo);
+
+        public enum MouseEventFlags : uint
+        {
+            LEFTDOWN = 0x00000002,
+            LEFTUP = 0x00000004,
+            MIDDLEDOWN = 0x00000020,
+            MIDDLEUP = 0x00000040,
+            MOVE = 0x00000001,
+            ABSOLUTE = 0x00008000,
+            RIGHTDOWN = 0x00000008,
+            RIGHTUP = 0x00000010,
+            WHEEL = 0x00000800,
+            XDOWN = 0x00000080,
+            XUP = 0x00000100
+        }
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            tmrSystem.Enabled = true;
+
+        }
+
+        /// <summary>
+        /// Simulates a mouse click
+        /// </summary>
+        private void MouseClik()
+        {
+            mouse_event((uint)MouseEventFlags.LEFTDOWN, 0, 0, 0, 0);
+            Thread.Sleep((new Random()).Next(20, 30));
+            mouse_event((uint)MouseEventFlags.LEFTUP, 0, 0, 0, 0);
+            mouse_event((uint)MouseEventFlags.LEFTDOWN, 0, 0, 0, 0);
+            Thread.Sleep((new Random()).Next(20, 30));
+            mouse_event((uint)MouseEventFlags.LEFTUP, 0, 0, 0, 0);
+        }
+
+        /// <summary>
+        /// Takes a snapshot of the screen
+        /// </summary>
+        /// <returns>A snapshot of the screen</returns>
+        private Bitmap Screenshot()
+        {
+
+
+
+            Bitmap captureBitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width / 3, Screen.PrimaryScreen.Bounds.Height / 2, PixelFormat.Format32bppArgb);
+            Rectangle captureRectangle = Screen.AllScreens[0].Bounds;
+            Graphics captureGraphics = Graphics.FromImage(captureBitmap);
+            captureGraphics.CopyFromScreen(Screen.PrimaryScreen.Bounds.Height/2, 200, 0, 0, captureRectangle.Size);
+            
+            //captureGraphics.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureRectangle.Size);    orjinal
+
+
+
+
+            // return the screenshot
+            return captureBitmap;
+        }
+
+        /// <summary>
+        /// Find the location of a bitmap within another bitmap and return if it was successfully found
+        /// </summary>
+        /// <param name="bmpNeedle">The image we want to find</param>
+        /// <param name="bmpHaystack">Where we want to search for the image</param>
+        /// <param name="location">Where we found the image</param>
+        /// <returns>If the bmpNeedle was found successfully</returns>
+        private bool FindBitmap(Bitmap bmpNeedle, Bitmap bmpHaystack, out Point location)
+        {
+            for (int outerX = 0; outerX < bmpHaystack.Width - bmpNeedle.Width; outerX++)
+            {
+                for (int outerY = 0; outerY < bmpHaystack.Height - bmpNeedle.Height; outerY++)
+                {
+                    for (int innerX = 0; innerX < bmpNeedle.Width; innerX++)
+                    {
+                        for (int innerY = 0; innerY < bmpNeedle.Height; innerY++)
+                        {
+                            Color cNeedle = bmpNeedle.GetPixel(innerX, innerY);
+                            Color cHaystack = bmpHaystack.GetPixel(innerX + outerX, innerY + outerY);
+
+                            if (cNeedle.R != cHaystack.R || cNeedle.G != cHaystack.G || cNeedle.B != cHaystack.B)
+                            {
+                                goto notFound;
+                            }
+                        }
+                    }
+                    location = new Point(outerX, outerY);
+                    return true;
+                    notFound:
+                    continue;
+                }
+            }
+            location = Point.Empty;
+            return false;
+        }
+
+        private void tmrSystem_Tick(object sender, EventArgs e)
+        {
+            Bitmap bmpScreenshot = Screenshot();
+            Point location;
+
+            this.BackgroundImage = bmpScreenshot;
+
+            bool success = FindBitmap(Properties.Resources.bmpLogin, bmpScreenshot, out location);
+
+            if (success == true)
+            {
+                listIslemler.Items.Add("Login Algılandı");
+                return;
+            }
+
+
+
+
+
+            bool successOTP = FindBitmap(Properties.Resources.bmpOtp, bmpScreenshot, out location);
+
+            if (successOTP == true)
+            {
+                bool otpGirilmedi = FindBitmap(Properties.Resources.otpGirilmedi, bmpScreenshot, out location);
+
+                if (otpGirilmedi == true)
+                {
+                    listIslemler.Items.Add("Otp Girilmedi");
+                    return;
+                }
+                else
+                {
+                    bmpScreenshot.Save(Application.StartupPath + "\\Otp.bmp");
+
+
+                    listIslemler.Items.Add("Otp Girildi");
+                    return;
+                }
+                
+            }
+
+
+
+
+
+
+
+
+
+            //Cursor.Position = location;
+        }
+
+    }
+}
